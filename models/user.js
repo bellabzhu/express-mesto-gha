8de751e36e98d9b7caec0bc5f3ea35db82725b10
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-02-11 21:11:31 +0400

    start 14th sprint, add default values for userSchema

--- models/user.js
+++ models/user.js
@@ -9,17 +9,20 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
     required: true,
     validate: URLvalidator,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
