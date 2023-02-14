ffdb525a919668eff01dd255aefa6167185acf99
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-02-14 16:22:56 +0400

    request validation with celebrate and joi

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const { celebrate, Joi } = require('celebrate');
 const { limiter } = require('./middlewares/limiter');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const { NOT_FOUND, OK } = require('./utils/errors');
 const { auth } = require('./middlewares/auth');
+const { regexURL } = require('./utils/constants');
 
 const { PORT = 3000, MONGODB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 mongoose.set('strictQuery', true);
@@ -38,8 +40,23 @@ async function start() {
 
 start();
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().min(8).required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(regexURL),
+    email: Joi.string().required().email(),
+    password: Joi.string().min(8).required(),
+  }),
+}), createUser);
+
 app.use(auth);
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
