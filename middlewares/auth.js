1866fa8029c830a624d1ec0009ae07dafd3a36df
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-02-16 15:32:48 +0400

    fix auth error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,6 @@ const Error401 = require('../errors/Error401');
 
 const auth = async (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) {
-    next(new Error401('Необходима авторизация.'));
-    return;
-  }
   let payload;
   try {
     payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
